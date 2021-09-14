420bd35c1d9ea8da73e277e1db3c53d531ed8d37
Dskunjava2 <90669382+Dskunjava2@users.noreply.github.com>
2021-09-14 10:52:46 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,19 +6,19 @@ class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
+Bot Creator - @DS_KUNJAVA
+Bot Editor  - @DS_KUNJAVA
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
-
-For More Details Click Help Button Below..
-@CrazyBotsz
+വെറുതെ നോക്കി സമയം കളയണ്ട @movie_house2 ഗ്രൂപ്പിൽ മാത്രേമേ ഈ ബോട്ട് ഉപയോഗിക്കാൻ പറ്റുകയുള്ളു..!😜😜
+━═━═━═━═━═━═━═━═━═
+@movie_house2
 </i>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+-> Any Dought Ask - @DS_KUNJAVA
 </i>
 
 <b>Bot Commands (Works Only In Groups) :</b>
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@ds_movies1
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/DS_KUNJAVA>Kunjava</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Dskunjava2/Adv-Auto-Filter-Bot-V2">Click Me</a></i>
 """
