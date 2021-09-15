1e522c06af569f0a59d96e384411874488eb35f6
DS KUNJAVA <90669382+Dskunjava2@users.noreply.github.com>
2021-09-15 13:22:56 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -16,39 +16,16 @@ Bot Editor  - @DS_KUNJAVA
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b><i><u>I Am A Power🔥!?</u></i></b>
 
 <i>
    -> Any Dought Ask - @DS_KUNJAVA
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-ഇത്രേ ഉള്ളു എന്നാ സമയം കളയണ്ട വേഗം ചാനലിലും കേറിക്കോ..!😋
+           
+ എന്നാ സമയം കളയണ്ട വേഗം ചാനലിലും കേറിക്കോ..!😋
 @ds_movies1 
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Night Fury 🖤</code>
     
 <b>➥ Creator</b> : <b><i><a href="https://t.me/DS_KUNJAVA>Kunjava</a></i></b>
 
@@ -56,5 +33,5 @@ Bot Editor  - @DS_KUNJAVA
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/Dskunjava2/Adv-Auto-Filter-Bot-V2">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Dskunjava2/Adv-Auto-Filter-Bot-V2">Click Here</a></i>
 """
